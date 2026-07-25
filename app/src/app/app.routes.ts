c897b40c0360fa9f57890906a816f7a30692c78a
paraizovinicius <paraizovinicius@gmail.com>
2026-07-25 16:46:14 +0200

    frontend: modularize pages (AI)

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		pathMatch: 'full',
+		loadComponent: () => import('./pages/home/home').then((m) => m.HomePage),
+	},
+	{
+		path: 'restaurants',
+		loadComponent: () => import('./pages/restaurants/restaurants').then((m) => m.RestaurantsPage),
+	},
+	{
+		path: 'profile',
+		loadComponent: () => import('./pages/profile/profile').then((m) => m.ProfilePage),
+	},
+	{
+		path: '**',
+		redirectTo: '',
+	},
+];
---
+++ app/src/app/pages/restaurants/restaurants.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurants-page',
+  standalone: true,
+  templateUrl: './restaurants.html',
+  styleUrl: './restaurants.css'
+})
+export class RestaurantsPage {}
