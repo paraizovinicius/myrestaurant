c8049c94d722cda94b35e21d4915ef0fd7143c64
paraizovinicius <paraizovinicius@gmail.com>
2026-07-25 23:40:08 +0200

    frontend: add footer

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -9,4 +9,5 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('MyRestaurant');
+  protected readonly currentYear = new Date().getFullYear();
 }
